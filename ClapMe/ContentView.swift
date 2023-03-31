c0cf07304df4415d0d4c72c83c4e63924ac48719
Aleksej Shapran <113884557+lepranby@users.noreply.github.com>
2023-03-31 21:56:47 +0300

    Forgot about scrollView.

--- ClapMe/ContentView.swift
+++ ClapMe/ContentView.swift
@@ -11,50 +11,52 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            
             MapView()
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 335)
-
+            
             CircleImage()
                 .offset(x: 92, y: -150)
                 .padding(.bottom, -220)
-
-            VStack(alignment: .leading) {
-                Text("Starbucks Coffee")
-                    .font(.title)
-                HStack {
-                    Text("Friedtich Straße 96 IHZ, Berlin")
-                    Spacer()
-                    Text("Deutchland")
-                }
-                .font(.callout)
-                .foregroundColor(.secondary)
-                .padding(0)
-
-                Divider()
-
-                Text("Über diesen Ort:")
-                    .font(.title2)
-                Text("Kaffeehauskette aus Seattle, bekannt für ihre unverkennbaren Röstungen, leichten Snacks und WLAN-Verfügbarkeit.")
-                    .font(.system(.callout))
+            
+            ScrollView {
                 
-                Divider()
-                
-                HStack {
-                    Button("zu Favoriten hinzufügen", action: emptyExit)
-                        .foregroundColor(.white)
-                        .buttonStyle(.borderedProminent)
-                        .controlSize(.large)
-                    Button("Einchecken", action: emptyExit)
-                        .foregroundColor(.blue)
-                        .buttonStyle(.bordered)
-                        .controlSize(.large)
+                VStack(alignment: .leading) {
+                    Text("Starbucks Coffee")
+                        .font(.title)
+                    HStack {
+                        Text("Friedtich Straße 96 IHZ, Berlin")
+                        Spacer()
+                        Text("Deutchland")
+                    }
+                    .font(.callout)
+                    .foregroundColor(.secondary)
+                    .padding(0)
+                    
+                    Divider()
+                    
+                    Text("Über diesen Ort:")
+                        .font(.title2)
+                    Text("Kaffeehauskette aus Seattle, bekannt für ihre unverkennbaren Röstungen, leichten Snacks und WLAN-Verfügbarkeit.")
+                        .font(.system(.callout))
+                    
+                    Divider()
+                    
+                    HStack {
+                        Button("zu Favoriten hinzufügen", action: emptyExit)
+                            .foregroundColor(.white)
+                            .buttonStyle(.borderedProminent)
+                            .controlSize(.large)
+                        Button("Einchecken", action: emptyExit)
+                            .foregroundColor(.blue)
+                            .buttonStyle(.bordered)
+                            .controlSize(.large)
+                    }
                 }
+                .padding(14)
+                
+                Spacer()
             }
-            .padding(14)
-
-            Spacer()
         }
     }
 }
